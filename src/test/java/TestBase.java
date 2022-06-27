12367e914394291681c57980d3161287cb8e9521
Lena Masloboishchikova <perepechkinaEV@yandex.ru>
2022-06-27 17:55:07 +0300

    add attach

--- src/test/java/TestBase.java
+++ src/test/java/TestBase.java
@@ -1,9 +1,15 @@
+import attach.Attach;
 import com.codeborne.selenide.Configuration;
 import com.codeborne.selenide.logevents.SelenideLogger;
 import config.WebDriverConfig;
+import io.qameta.allure.selenide.AllureSelenide;
 import org.aeonbits.owner.ConfigFactory;
+import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeAll;
 import org.openqa.selenium.remote.DesiredCapabilities;
+
+
+import static com.codeborne.selenide.Selenide.closeWebDriver;
 
 public class TestBase {
     public static WebDriverConfig webConfig = ConfigFactory.create(WebDriverConfig.class, System.getProperties());
@@ -11,19 +17,32 @@ public class TestBase {
 
     @BeforeAll
     static void beforeAll() {
-
+        SelenideLogger.addListener("AllureSelenide", new AllureSelenide());
 
         Configuration.baseUrl = webConfig.getBaseUrl();
         Configuration.browserSize = webConfig.browserSize();
         Configuration.browser = String.valueOf(webConfig.getBrowser());
         Configuration.browserVersion = webConfig.browserVersion();
 
-        if(!webConfig.getRemoteUrl().equals("")) {
+        if (!webConfig.getRemoteUrl().equals("")) {
             DesiredCapabilities capabilities = new DesiredCapabilities();
             capabilities.setCapability("enableVNC", true);
             capabilities.setCapability("enableVideo", true);
             Configuration.browserCapabilities = capabilities;
             Configuration.remote = webConfig.getRemoteUrl();
         }
+    }
+
+
+    @AfterEach
+    void addAttachments() {
+        Attach.screenshotAs("Last screenshot");
+        Attach.pageSource();
+        Attach.browserConsoleLogs();
+        if (!webConfig.getRemoteUrl().equals("")) {
+            Attach.addVideo();
+        }
+        closeWebDriver();
+
     }
 }
